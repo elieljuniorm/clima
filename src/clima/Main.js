3347eeef8057c23a12f253c46c45ee94b4e3dd43
Eliel Maia <elieljuniorm@gmail.com>
2022-05-10 14:41:07 -0300

    registrando api e utilizando dados

--- src/clima/Main.js
+++ src/clima/Main.js
@@ -1,13 +1,30 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 function Main() {
     /* usestate roda para pedir a localização */
     const [location,setLocation] = useState(false);
 
+    /* armazena os dados da api */
+    const [weather , setWeather] = useState(false);
+
+    let getWeather = async (lat,long) => {
+        let res = await axios.get("https://api.openweathermap.org/data/2.5/weather" , {
+            params: {
+                lat: lat,
+                lon: long,
+                appid: process.env.REACT_APP_OPEN_WEATHER_KEY,
+                lang: 'pt',
+                units: 'metric'
+            }
+        });
+        setWeather(res.data)
+    }
+
     /* useeffect roda assim que o app for aberto pedindo a localização */
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position.coords.latitude, position.coords.longitude);
+            getWeather(position.coords.latitude, position.coords.longitude);
             setLocation(true);
         })
     }, [/* usado para chamar somente quando for aberto */]);
@@ -16,22 +33,24 @@ function Main() {
         return(
             <>Você precisa Habilitar a localização no browser!</>
         )
+    }else if(weather == false){
+        <>Carregando o clima...</>
     }else{
         return(
             <>
-                <h3>Clima nas suas Coordenadas: Exemplo</h3>
+                <h3>Clima nas suas Coordenadas ({weather['weather'][0]['description']})</h3>
                 <hr/>
                 <ul>
 
-                    <li>Temperatura atual: x°</li>
+                    <li>Temperatura atual: {weather['main'][0]['temp']}°</li>
 
-                    <li>Temperatura máxima: x°</li>
+                    <li>Temperatura máxima: {weather['main'][0]['temp_max']}°</li>
 
-                    <li>Temperatura mínima: x°</li>
+                    <li>Temperatura mínima: {weather['main'][0]['temp_min']}°</li>
 
-                    <li>Pressão: X hpa</li>
+                    <li>Pressão: {weather['main'][0]['pressure']} hpa</li>
 
-                    <li>Umidade realativa: x%</li>
+                    <li>Umidade realativa: {weather['main'][0]['humidity']}%</li>
 
                 </ul>
             </>
